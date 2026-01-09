db27d2dbb19140c6f2a80fe37f08dc02a112a29f
Nicolas <nicolas.programador@gmail.com>
2026-01-09 19:56:43 -0300

    validaciones y modularizacion del codigo

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.get('/api/usuarios', (req,res) => {
 
 
 app.get('/api/usuarios/:id',(req,res) => {
-    let usuario = usuarios.find((u) => {
-        return u.id === parseInt(req.params.id);
-    });
+    let usuario = existeUsuario(req.params.id);
     if(!usuario){
         return res
             .status(404)
@@ -43,12 +41,8 @@ app.get('/api/usuarios/:id',(req,res) => {
 })
 
 app.post('/api/usuarios', (req,res) => {
-
-    const schema = Joi.object({
-        nombre: Joi.string().min(3).max(30).required()
-    });
-
-    const { error , value } = schema.validate({ nombre: req.body.nombre });
+    
+    const { error , value } = validarUsuario(req.body.nombre);
     
     if(!error){
         const usuario = {
@@ -63,16 +57,14 @@ app.post('/api/usuarios', (req,res) => {
 });
 
 app.put('/api/usuarios/:id', (req,res) => {
-    //Encontrar si existe el usuario
-    let usuario = usuarios.find((u) => ( u.id === parseInt(req.params.id)));
-    if(!usuario) return res.status(404).send('El usuario no fue encontrado');
-    
-    const schema = Joi.object({
-        nombre: Joi.string().min(3).max(30).required()
-    });
+    //Encontrar si existe el usuario    
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario){
+        res.status(404).send('El usuario no fue encontrado');
+        return;
+    }        
 
-    const { error , value } = schema.validate({ nombre: req.body.nombre });
-    
+    const { error , value } = validarUsuario(req.body.nombre);    
     if(error){
         const mensaje = error.details[0].message;
         res.status(400).send(mensaje);
@@ -88,3 +80,15 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 }); //Puerto
+
+
+function existeUsuario(id){
+    return usuarios.find((u) => u.id === parseInt(id));
+}
+    
+function validarUsuario(nom){
+    const schema = Joi.object({
+        nombre: Joi.string().min(3).max(30).required()
+    });
+    return schema.validate({ nombre: nom });
+}
