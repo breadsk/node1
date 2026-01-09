be0d22ec1906006c63696f46e5ee76d6be0df64f
Nicolas <nicolas.programador@gmail.com>
2026-01-09 17:45:35 -0300

    cambios e instalaciones

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const usuarios = [
+    {id:1,nombre: 'Katherine'},
+    {id:2,nombre: 'Nicolas'},
+    {id:3,nombre: 'Jeremy'},
+    {id:4,nombre: 'Hernan'},
+]
 
 app.get('/', (req,res) => {
     res.send('Hola Mundo desde Express');
@@ -10,6 +16,25 @@ app.get('/api/usuarios', (req,res) => {
     res.send(['katherine','nicolas','hernan','jeremy']);
 });
 
-app.listen(3000, () => {
-    console.log('Servidor escuchando en el puerto 3000');
+
+app.get('/api/usuarios/:id',(req,res) => {
+    let usuario = usuarios.find((u) => {
+        return u.id === parseInt(req.params.id);
+    });
+    if(!usuario){
+        return res
+            .status(404)
+            .send('El usuario no fue encontrado');
+    }
+    res.send({
+        id: usuario.id,
+        usuario,
+        statusCode : 200
+    });
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
 }); //Puerto
